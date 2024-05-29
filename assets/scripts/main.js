5aa29d7b0d1d88e06725778ff35a557377e8b7fe
Tianshi-Gong <tgong@ucsd.edu>
2024-05-28 22:55:13 -0700

    A&B

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -54,6 +54,15 @@ function initializeServiceWorker() {
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js').then((registration) => {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch((error) => {
+            console.error('ServiceWorker registration failed: ', error);
+        });
+    });
+}
 }
 
 /**
@@ -100,6 +109,27 @@ async function getRecipes() {
   //            resolve() method.
   // A10. TODO - Log any errors from catch using console.error
   // A11. TODO - Pass any errors to the Promise's reject() function
+  let storedRecipes = localStorage.getItem('recipes');
+  if (storedRecipes) {
+      return JSON.parse(storedRecipes);
+  }
+  let recipes = [];
+  return new Promise(async (resolve, reject) => {
+      for (let url of RECIPE_URLS) {
+          try {
+              let response = await fetch(url);
+              let recipe = await response.json();
+              recipes.push(recipe);
+              if (recipes.length === RECIPE_URLS.length) {
+                  saveRecipesToStorage(recipes);
+                  resolve(recipes);
+              }
+          } catch (error) {
+              console.error(error);
+              reject(error);
+          }
+      }
+  });
 }
 
 /**
